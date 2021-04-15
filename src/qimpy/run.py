77a8d6537146fdef9133151f797e8be4a8f7f189
Ravishankar Sundararaman <shankar.progs2@gmail.com>
2021-04-15 19:05:27 -0400

    Added a placeholder main script to the root directory

---
+++ src/qimpy/run.py
@@ -0,0 +1,16 @@
+if __name__ == "__main__":
+    print('''
+        This will serve as the main QimPy executable.
+        After installation, we should be able to run as
+        
+            python -m qimpy.run <arguments>
+            
+        from anywhere in the python path. Without install,
+        this will only work from the src/ path. During
+        development, set up for testing by running:
+
+            python setup.py develop --user
+
+        in the root directory of the repository.
+    ''')
+
