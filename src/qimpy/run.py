f0dbb41b5074c0b609590ab01bec818e7304a8b0
Ravishankar Sundararaman <shankar.progs2@gmail.com>
2021-04-16 17:07:44 -0400

    Added command-line parser to pick input file. Moved test input to tests/ directory and modified it.

--- src/qimpy/run.py
+++ src/qimpy/run.py
@@ -1,42 +1,64 @@
 import yaml
 
-class  Qimpy(object):
-    def __init__(self, yamlDict): 
-        ''' 
-        Input: dictionary yamlDict generated from yaml read-in
-        Instantiates Qimpy object whose attributes are the keys of the yaml dict
-        see: https://stackoverflow.com/questions/1305532/convert-nested-python-dict-to-object
+
+class Qimpy(object):
+    def __init__(self, yamlDict):
         '''
-        for inputKeyword, inputValue in yamlDict.items(): # for key, value
-            if isinstance(inputValue, (list, tuple)): # is value a list or tuple?
-                setattr(self, inputKeyword, [Qimpy(x) if isinstance(x, dict) else x for x in inputValue])
+        Input: dictionary yamlDict generated from yaml read-in
+        Instantiates Qimpy object whose attributes are keys of yamlDict
+        see: https://stackoverflow.com/questions/1305532/
+            convert-nested-python-dict-to-object
+        '''
+        for inputKeyword, inputValue in yamlDict.items():  # for key, value
+            if isinstance(inputValue, (list, tuple)):
+                setattr(
+                    self, inputKeyword,
+                    [(Qimpy(x) if isinstance(x, dict) else x)
+                        for x in inputValue])
             else:
-                setattr(self, inputKeyword, Qimpy(inputValue) if isinstance(inputValue, dict) else inputValue)
-           
+                setattr(
+                    self, inputKeyword,
+                    (Qimpy(inputValue) if isinstance(inputValue, dict)
+                        else inputValue))
 
 
 if __name__ == "__main__":
-    print('''
-        This will serve as the main QimPy executable.
-        After installation, we should be able to run as
-        
-            python -m qimpy.run <arguments>
-            
-        from anywhere in the python path. Without install,
-        this will only work from the src/ path. During
-        development, set up for testing by running:
 
-            python setup.py develop --user
-
-        in the root directory of the repository.
-    ''')
-
-    fileQimpy = 'qimpy_input_test.yaml'
-    with open(fileQimpy) as f:
-        allInputs = yaml.safe_load(f) # dictionary of inputs from yaml file
+    # Parse the commandline arguments:
+    import argparse
+    parser = argparse.ArgumentParser(
+        description='Run a QimPy calculation from an input file')
+    parser.add_argument(
+        'input_file',
+        help='input file in YAML format')
+    parser.add_argument(
+        '-o', '--output-file',
+        help='output file (stdout if unspecified)')
+    parser.add_argument(
+        '-c', '--cores', type=int,
+        help='number of cores per process (overridden by SLURM)')
+    parser.add_argument(
+        '-n', '--dry-run', action='store_true',
+        help='quit after initialization (to check input file)')
+    parser.add_argument(
+        '-d', '--no-append', action='store_true',
+        help='overwrite output file instead of appending')
+    parser.add_argument(
+        '-m', '--mpi-log',
+        help='file prefix for debug logs from other MPI processes')
+    parser.add_argument(
+        '-v', '--version', action='store_true',
+        help='print version information and quit')
+    parser.add_argument(
+        '-V', '--verbose', action='store_true',
+        help='print extra information in log for debugging')
+    args = parser.parse_args()
+    print(args)
+    
+    with open(args.input_file) as f:
+        allInputs = yaml.safe_load(f)  # dictionary of inputs from yaml file
 
     qimpy = Qimpy(allInputs)
     print(qimpy.__dict__)
-    print(qimpy.Lattice)
-    print(qimpy.IonCoordinates)
-
+    print(qimpy.lattice)
+    print(qimpy.ions)
