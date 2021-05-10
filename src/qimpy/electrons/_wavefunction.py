14113406382dd0a290768c4e8477d99e0f0a3fa6
Ravishankar Sundararaman <shankar.progs2@gmail.com>
2021-05-10 12:47:15 -0400

    Switched Wavefunction out of dataclass to make it easier to read (not much gain of auto-init, repr in this case anyway)

--- src/qimpy/electrons/_wavefunction.py
+++ src/qimpy/electrons/_wavefunction.py
@@ -1,28 +1,46 @@
-from dataclasses import dataclass, InitVar
-from ._basis import Basis
 import qimpy as qp
 import torch
 
 
-@dataclass
 class Wavefunction:
     '''TODO: document class Wavefunction'''
 
-    basis: Basis
-    coeff: torch.Tensor = None  # coefficients in specified basis
-    proj: list = None  # projections to pseudopotential projectors
-    is_split_basis: bool = True  # whether MPI-split over basis
-
-    # Variables used for initialization only:
-    n_bands: InitVar[int] = 0
-    n_spins: InitVar[int] = 0  # optionally override n_spins from basis
-    n_spinor: InitVar[int] = 0  # optionally override n_spinor from basis
-    randomize: InitVar[bool] = False  # optionally random instead of zeroes
-
-    def __post_init__(self, n_bands, n_spins, n_spinor, randomize):
-        '''Create wavefunctions with specified number of bands and
-        optionally randomize them  (only if coeff is None).
-        Note that this does not initialize the projections.'''
+    def __init__(self, basis, coeff=None, proj=None, is_split_basis=True,
+                 n_bands=0, n_spins=0, n_spinor=0, randomize=False):
+        '''
+        Parameters
+        ----------
+        basis: qimpy.electrons.Basis
+            Basis set for wavefunction
+        coeff: torch.Tensor
+            Wavefunction coefficients in specified basis
+        proj: list of torch.Tensor
+            Projections to pseudopotential projectors for each ion species
+        is_split_basis: bool, default: True
+            Whether the wavefunctions are MPI-split over basis (default).
+            If false, the wavefunctions have been rearranged to bring
+            all basis coefficients together (splitting over bands instead)
+        n_bands: int, default: 0
+            Number of bands to initialize wavefunctions for.
+            Used only if coeff is None, and does not initialize proj
+        n_spins: int, default: 0
+            If non-zero, override the number of spin channels in
+            new wavefunctions (instead of the value in basis).
+            Used only if coeff is None, and n_bands is non-zero
+        n_spinor: int, default: 0
+            If non-zero, override the number of spinor components
+            in new wavefunctions (instead of the value in basis).
+            Used only if coeff is None, and n_bands is non-zero
+        randomize: bool, default: False
+            If true, randomize new wavefunctions (see :meth:`randomize`),
+            else initialize them to zero (default).
+            Used only if coeff is None, and n_bands is non-zero
+        '''
+        self.basis = basis
+        self.coeff = coeff
+        self.proj = proj
+        self.is_split_basis = is_split_basis
+        # Optional initialization:
         if (self.coeff is None) and n_bands:
             basis = self.basis
             nk_mine = basis.kpoints.n_mine
