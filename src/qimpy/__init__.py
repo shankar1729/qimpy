ae41f726555b3ba43efc4ed5cedb74694ed11eb9
Ravishankar Sundararaman <shankar.progs2@gmail.com>
2021-07-11 19:21:50 -0400

    Renamed _input to _construct to better correspond to current functionality

--- src/qimpy/__init__.py
+++ src/qimpy/__init__.py
@@ -7,7 +7,7 @@ __all__ = [
 
 # Module import definition
 from mpi4py import MPI
-from ._input import Constructable, ConstructOptions
+from ._construct import Constructable, ConstructOptions
 from ._energy import Energy
 from . import utils
 from . import ions
