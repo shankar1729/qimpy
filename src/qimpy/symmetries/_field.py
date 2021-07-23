813302ad9c17eb3002ddb8ad06aa85664f3fa2e0
Ravishankar Sundararaman <shankar.progs2@gmail.com>
2021-07-23 10:09:45 -0400

    Top-level outline of symmetrizer initialization and application logic

--- src/qimpy/symmetries/_field.py
+++ src/qimpy/symmetries/_field.py
@@ -6,15 +6,33 @@ if TYPE_CHECKING:
 
 
 class FieldSymmetrizer:
-    """Space group symmetrization of reciprocal-space :class:`Field`'s."""
+    """Space group symmetrization of reciprocal-space :class:`FieldH`'s."""
     grid: 'Grid'
 
     def __init__(self, grid: 'Grid') -> None:
         """Initialize symmetrization for fields on `grid`."""
         self.grid = grid
-        qp.log.info('Will initialize symmetrization here.')
+        # Get reciprocal mesh of all processes:
+        # TODO
+
+        # Find symmetry-reduced set:
+        # TODO
+
+        # Set up indices, phases of orbits of each point in reduced set:
+        # TODO
+
+        # Set up MPI split over orbits:
+        # TODO
 
     def __call__(self, v: 'FieldH') -> None:
         """Symmetrize field `v` in-place."""
         grid = self.grid
         assert v.grid == grid
+        # Collect data by orbits, transfering over MPI as needed:
+        # TODO
+
+        # Symmetrize in each orbit:
+        # TODO
+
+        # Set results back to original grid, transfering over MPI as needed:
+        # TODO
