f292abb4292235ab8246bb3f4db63d7401064606
Ravishankar Sundararaman <shankar.progs2@gmail.com>
2021-10-26 12:26:31 -0400

    Updated requirements to get pip install to work correctly

--- setup.py
+++ setup.py
@@ -1,17 +1,25 @@
 import setuptools
-import versioneer
 from torch.utils import cpp_extension
+import sys
+import os
+
+
+def get_version_cmdclass():
+    import versioneer
+    return versioneer.get_version(), versioneer.get_cmdclass()
+
+
+sys.path.append(os.path.dirname(__file__))  # needed for versioneer
+version, cmdclass = get_version_cmdclass()
+cmdclass['build_ext'] = cpp_extension.BuildExtension
 
 ext_modules = [
     cpp_extension.CppExtension(
         'qimpy.utils._bufferview',
         ['src/qimpy/utils/_bufferview.cpp'])]
 
-cmdclass = versioneer.get_cmdclass()
-cmdclass['build_ext'] = cpp_extension.BuildExtension
-
 setuptools.setup(
-    version=versioneer.get_version(),
+    version=version,
     cmdclass=cmdclass,
     ext_modules=ext_modules
 )
