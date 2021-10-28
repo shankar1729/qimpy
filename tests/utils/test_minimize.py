fdbad3c24792a4cf0467269d3dff87baf7b18750
Ravishankar Sundararaman <shankar.progs2@gmail.com>
2021-10-27 21:00:19 -0400

    Added MPI version of minimize test

--- tests/utils/test_minimize.py
+++ tests/utils/test_minimize.py
@@ -31,60 +31,85 @@ class RandomFunction(qp.utils.Minimize[qp.grid.FieldR]):  # type: ignore
             extra_thresholds={"|grad|": 1e-8},
             method=method,
         )
-        lattice = qp.lattice.Lattice(rc=rc, system="Orthorhombic", a=1.0, b=1.0, c=10.0)
+        lattice = qp.lattice.Lattice(rc=rc, system="Orthorhombic", a=10.0, b=1.0, c=1.0)
         ions = qp.ions.Ions(rc=rc, pseudopotentials=[], coordinates=[])
         symmetries = qp.symmetries.Symmetries(rc=rc, lattice=lattice, ions=ions)
         grid = qp.grid.Grid(
             rc=rc,
             lattice=lattice,
             symmetries=symmetries,
-            shape=(1, 1, n_dim),
+            shape=(n_dim, 1, 1),
             comm=self.rc.comm,
         )
         self.grid = grid
-        x0 = torch.arange(n_dim, dtype=torch.float64, device=self.rc.device)
-        self.x0 = qp.grid.FieldR(grid, data=x0.view(grid.shape))
+        self.i0slice = slice(grid.split0.i_start, grid.split0.i_stop)
+        x0 = torch.arange(
+            self.i0slice.start,
+            self.i0slice.stop,
+            dtype=torch.float64,
+            device=self.rc.device,
+        )
+        self.x0 = qp.grid.FieldR(grid, data=x0.view(grid.shapeR_mine))
         self.E0 = -5.0
         torch.random.manual_seed(0)
         self.x = self.x0 + 0.1 * self.random_direction()
-        self.M = []
+        M_all = []  # full matrices
+        self.M = []  # process-divided matrix
         for i_M in range(2):
-            self.M.append(torch.randn((n_dim, n_dim), device=self.rc.device))
+            M_all.append(torch.randn((n_dim, n_dim), device=self.rc.device))
+            self.M.append(M_all[-1][:, self.i0slice])
         # Preconditioner (inexact inverse):
-        Kreg = 0.1 * (self.M[0] ** 2).sum() * torch.eye(n_dim, device=self.rc.device)
-        self.K = torch.linalg.inv(Kreg + self.M[0].T @ self.M[0])
+        Kreg = 0.1 * (M_all[0] ** 2).sum() * torch.eye(n_dim, device=self.rc.device)
+        self.K = torch.linalg.inv(Kreg + M_all[0].T @ M_all[0])[:, self.i0slice]
 
     def step(self, direction: qp.grid.FieldR, step_size: float) -> None:
         self.x += step_size * direction
 
     def compute(self, state, energy_only):  # type: ignore
         E = self.E0
-        E_x = torch.zeros_like(self.x0.data)
+        E_x = torch.zeros_like(self.x0.data.flatten())
         for i_M, M in enumerate(self.M):
-            v = M @ (self.x - self.x0).data[0, 0]
+            v = M @ (self.x - self.x0).data.flatten()  # partial results, full array
+            self.comm.Allreduce(qp.MPI.IN_PLACE, qp.utils.BufferView(v), qp.MPI.SUM)
             v_norm_sq = (v ** 2).sum().item()
             E += v_norm_sq ** (i_M + 1) * self.grid.dV
-            E_x[0, 0] += (2 * (i_M + 1) * (v_norm_sq ** i_M)) * (M.T @ v)
-        K_E_x = (self.K @ E_x[0, 0]).view(E_x.shape)
+            E_x += (2 * (i_M + 1) * (v_norm_sq ** i_M)) * (M.T @ v)
+        # Apply preconditioner:
+        K_E_x = self.K @ E_x  # partial results, full array
+        self.comm.Allreduce(qp.MPI.IN_PLACE, qp.utils.BufferView(K_E_x), qp.MPI.SUM)
+        K_E_x = K_E_x[self.i0slice]  # full results, partial array
         state.energy["E"] = E
         if not energy_only:
-            state.gradient = qp.grid.FieldR(self.grid, data=E_x)
-            state.K_gradient = qp.grid.FieldR(self.grid, data=K_E_x)
+            state.gradient = qp.grid.FieldR(
+                self.grid, data=E_x.view(self.grid.shapeR_mine)
+            )
+            state.K_gradient = qp.grid.FieldR(
+                self.grid, data=K_E_x.view(self.grid.shapeR_mine)
+            )
             state.extra = [state.gradient.norm()]
 
     def random_direction(self) -> qp.grid.FieldR:
         data = torch.randn(self.grid.shape, device=self.rc.device)
-        return qp.grid.FieldR(self.grid, data=data)
+        return qp.grid.FieldR(self.grid, data=data[self.i0slice])
 
 
-@pytest.mark.mpi_skip
-@pytest.mark.parametrize(
-    "n_dim, method", [(10, "cg"), (100, "cg"), (10, "l-bfgs"), (100, "l-bfgs")]
-)
+@pytest.mark.parametrize("n_dim, method", [(10, "cg"), (100, "cg"), (100, "l-bfgs")])
 def test_minimize(n_dim: int, method: str, rc: qp.utils.RunConfig):
     n_dim = 100
     method = "cg"
     rf = RandomFunction(rc=rc, n_dim=n_dim, method=method)
-    rf.finite_difference_test(rf.random_direction())
     E = float(rf.minimize())
     assert abs(E - rf.E0) < (n_dim * rf.energy_threshold)
+
+
+if __name__ == "__main__":
+
+    def main():
+        """Manually run a test with full output"""
+        qp.utils.log_config()
+        rc = qp.utils.RunConfig()
+        rf = RandomFunction(rc=rc, n_dim=100, method="cg")
+        rf.finite_difference_test(rf.random_direction())
+        rf.minimize()
+
+    main()
