37de9c8319364a0fb06c9ed9a6d7003e066fd0d6
Ravishankar Sundararaman <shankar.progs2@gmail.com>
2023-02-05 18:00:03 -0500

    Added optional thermostat and barostat degrees of freedom to geometry.gradient

--- src/qimpy/geometry/_gradient.py
+++ src/qimpy/geometry/_gradient.py
@@ -1,6 +1,6 @@
 import torch
 from dataclasses import dataclass
-from typing import Optional
+from typing import Optional, ClassVar
 
 
 @dataclass
@@ -9,54 +9,74 @@ class Gradient:
 
     ions: torch.Tensor  #: ionic gradient (forces)
     lattice: Optional[torch.Tensor] = None  #: lattice gradient (stress)
+    thermostat: Optional[torch.Tensor] = None  #: thermostat gradient (e.g. Nose-Hoover)
+    barostat: Optional[torch.Tensor] = None  #: barostat gradient (e.g. Nose-Hoover)
+    OPTIONAL_ATTRIBUTE_NAMES: ClassVar[set[str]] = {"lattice", "thermostat", "barostat"}
 
     def clone(self) -> "Gradient":
-        return Gradient(
-            ions=self.ions.clone().detach(),
-            lattice=(None if (self.lattice is None) else self.lattice.clone().detach()),
-        )
+        result = Gradient(ions=self.ions.clone().detach())
+        for attribute_name in Gradient.OPTIONAL_ATTRIBUTE_NAMES:
+            if (self_attribute := getattr(self, attribute_name)) is not None:
+                setattr(result, attribute_name, self_attribute.clone().detach())
+        return result
 
     def __add__(self, other: "Gradient") -> "Gradient":
-        return Gradient(
-            ions=(self.ions + other.ions),
-            lattice=(None if (self.lattice is None) else self.lattice + other.lattice),
-        )
+        result = Gradient(ions=(self.ions + other.ions))
+        for attribute_name in Gradient.OPTIONAL_ATTRIBUTE_NAMES:
+            if (self_attribute := getattr(self, attribute_name)) is not None:
+                other_attribute = getattr(other, attribute_name)
+                assert other_attribute is not None
+                setattr(result, attribute_name, self_attribute + other_attribute)
+        return result
 
     def __iadd__(self, other: "Gradient") -> "Gradient":
         self.ions += other.ions
-        if self.lattice is not None:
-            self.lattice += other.lattice
+        for attribute_name in Gradient.OPTIONAL_ATTRIBUTE_NAMES:
+            if (self_attribute := getattr(self, attribute_name)) is not None:
+                other_attribute = getattr(other, attribute_name)
+                assert other_attribute is not None
+                self_attribute += other_attribute
         return self
 
     def __sub__(self, other: "Gradient") -> "Gradient":
-        return Gradient(
-            ions=(self.ions - other.ions),
-            lattice=(None if (self.lattice is None) else self.lattice - other.lattice),
-        )
+        result = Gradient(ions=(self.ions - other.ions))
+        for attribute_name in Gradient.OPTIONAL_ATTRIBUTE_NAMES:
+            if (self_attribute := getattr(self, attribute_name)) is not None:
+                other_attribute = getattr(other, attribute_name)
+                assert other_attribute is not None
+                setattr(result, attribute_name, self_attribute - other_attribute)
+        return result
 
     def __isub__(self, other: "Gradient") -> "Gradient":
         self.ions -= other.ions
-        if self.lattice is not None:
-            self.lattice -= other.lattice
+        for attribute_name in Gradient.OPTIONAL_ATTRIBUTE_NAMES:
+            if (self_attribute := getattr(self, attribute_name)) is not None:
+                other_attribute = getattr(other, attribute_name)
+                assert other_attribute is not None
+                self_attribute -= other_attribute
         return self
 
     def __mul__(self, other: float) -> "Gradient":
-        return Gradient(
-            ions=(self.ions * other),
-            lattice=(None if (self.lattice is None) else self.lattice * other),
-        )
+        result = Gradient(ions=(self.ions * other))
+        for attribute_name in Gradient.OPTIONAL_ATTRIBUTE_NAMES:
+            if (self_attribute := getattr(self, attribute_name)) is not None:
+                setattr(result, attribute_name, self_attribute * other)
+        return result
 
     __rmul__ = __mul__
 
     def __imul__(self, other: float) -> "Gradient":
         self.ions *= other
-        if self.lattice is not None:
-            self.lattice *= other
+        for attribute_name in Gradient.OPTIONAL_ATTRIBUTE_NAMES:
+            if (self_attribute := getattr(self, attribute_name)) is not None:
+                self_attribute *= other
         return self
 
     def vdot(self, other: "Gradient") -> float:
         result = self.ions.flatten() @ other.ions.flatten()
-        if self.lattice is not None:
-            assert other.lattice is not None
-            result += self.lattice.flatten() @ other.lattice.flatten()
+        for attribute_name in Gradient.OPTIONAL_ATTRIBUTE_NAMES:
+            if (self_attribute := getattr(self, attribute_name)) is not None:
+                other_attribute = getattr(other, attribute_name)
+                assert other_attribute is not None
+                result += self_attribute.flatten() @ other_attribute.flatten()
         return float(result.item())
