30da1905f6eb1f2174d5429108362aa882f6ea35
Ravishankar Sundararaman <shankar.progs2@gmail.com>
2023-05-03 14:50:10 -0400

    Test visualization of quads

--- src/qimpy/transport/_geometry.py
+++ src/qimpy/transport/_geometry.py
@@ -1,7 +1,7 @@
 from __future__ import annotations
 from .. import TreeNode, rc
 from ..utils import CpPath
-from typing import Sequence, Union
+from typing import Sequence, Union, Any, Optional
 import numpy as np
 import torch
 
@@ -11,13 +11,15 @@ class Geometry(TreeNode):
 
     vertices: torch.Tensor  #: Cartesian coordinates of vertices (n_vertices x 2)
     edges: torch.Tensor  #: 0-based vertex indices and edge resolution (n_edges x 4)
+    quads: torch.Tensor  #: 0-based edge indices in each quad (n_quads x 4)
     edge_splines: list[QuadraticSpline]
 
     def __init__(
         self,
         *,
         vertices: Union[Sequence[Sequence[float]], np.ndarray, torch.Tensor],
-        edges: Union[Sequence[Sequence[float]], np.ndarray, torch.Tensor],
+        edges: Union[Sequence[Sequence[int]], np.ndarray, torch.Tensor],
+        quads: Union[Sequence[Sequence[int]], np.ndarray, torch.Tensor],
         checkpoint_in: CpPath = CpPath(),
     ):
         """
@@ -28,21 +30,19 @@ class Geometry(TreeNode):
         vertices
             :yaml:`Caretsian vertex coordinates (n_vertices x 2).`
         edges
-            :yaml:`Indices of vertices in edges (n_edges x 4.`
+            :yaml:`Indices of vertices in edges (n_edges x 4).`
             Each edge should have three 0-based indices into the vertices array,
             for the starting, end and mid point. The midpoint index can be set
             to -1 to make the edge linear (autocompute midpoint from extremes).
             The final entry is the number of subdivisions, or -1 for automatic
+        quads
+            :yaml:`Indices of edges in each quad (n_quads x 4).`
         """
         super().__init__()
 
-        self.vertices = torch.tensor(vertices, device=rc.device)
-        assert len(self.vertices.shape) == 2
-        assert self.vertices.shape[1] == 2
-
-        self.edges = torch.tensor(edges, device=rc.device, dtype=torch.int)
-        assert len(self.edges.shape) == 2
-        assert self.edges.shape[1] == 4
+        self.vertices = _make_check_tensor(vertices, (-1, 2))
+        self.edges = _make_check_tensor(edges, (-1, 4), dtype=torch.int)
+        self.quads = _make_check_tensor(quads, (-1, 4), dtype=torch.int)
 
         self.edge_splines = []
         for i0, i1, i_mid, n_points in self.edges.to(rc.cpu):
@@ -71,7 +71,6 @@ class QuadraticSpline:
         ).to(rc.device)
         rhs = torch.stack((v0, v_mid, v1))
         self.coeff = torch.linalg.solve(Lhs, rhs)
-        print(self.coeff)
 
     def value(self, x: torch.Tensor) -> torch.Tensor:
         """Get spline coordinate values for a sequence of x."""
@@ -83,3 +82,16 @@ class QuadraticSpline:
         return self.value(
             torch.arange(self.n_points + 1, dtype=torch.double, device=rc.device)
         )
+
+
+def _make_check_tensor(
+    data: Union[Sequence[Sequence[Any]], np.ndarray, torch.Tensor],
+    dims: Sequence[int],
+    dtype: Optional[torch.dtype] = None,
+) -> torch.Tensor:
+    result = torch.tensor(data, device=rc.device, dtype=dtype)
+    assert len(result.shape) == len(dims)
+    for result_shape_i, dim_i in zip(result.shape, dims):
+        if dim_i >= 0:
+            assert result_shape_i == dim_i
+    return result
--- src/qimpy/transport/test_geometry.py
+++ src/qimpy/transport/test_geometry.py
@@ -1,21 +1,70 @@
 from . import Geometry
 import matplotlib.pyplot as plt
+import torch
 
 
 def test_geometry():
-    vertices = [[0.0, 0.0], [10.0, 0.0], [15.0, 20.0], [0.0, 15.0], [2.0, 7.0]]
+    vertices = [
+        [0.0, 0.0],
+        [3.0, 0.0],
+        [11.0, 0.0],
+        [17.0, 20.0],
+        [10.0, 18.0],
+        [3.0, 16.0],
+        [0.0, 15.0],
+        [5.0, 12.0],
+        [3.0, 5.0],
+        [9.0, 8.0],
+        [3.0, 9.0],
+        [7.0, 4.0],
+        [8.0, 11.0],
+    ]
     edges = [
         [0, 1, -1, 15],
         [1, 2, -1, 20],
         [2, 3, -1, 15],
-        [3, 0, 4, 20],
+        [3, 4, -1, 15],
+        [4, 5, -1, 15],
+        [5, 6, -1, 15],
+        [6, 0, -1, 15],
+        [7, 8, 10, 10],
+        [8, 9, 11, 10],
+        [9, 7, 12, 10],
+        [7, 5, -1, 10],
+        [8, 6, -1, 10],
+        [8, 1, -1, 10],
+        [9, 2, -1, 10],
+        [9, 4, -1, 10],
     ]
-    geometry = Geometry(vertices=vertices, edges=edges)
+    quads = [
+        [0, 12, 11, 6],
+        [1, 13, 8, 12],
+        [2, 3, 14, 13],
+        [9, 14, 4, 10],
+        [7, 10, 5, 11],
+    ]
+    geometry = Geometry(vertices=vertices, edges=edges, quads=quads)
 
     plt.figure()
-    for spline in geometry.edge_splines:
+    for i_edge, spline in enumerate(geometry.edge_splines):
         x, y = spline.points.T
         plt.plot(x, y, marker="+")
+        n_label = spline.n_points // 3  # avoid collision with vertex at midpoint
+        plt.text(x[n_label], y[n_label], f"E{i_edge}")
+
+    for i_vertex, (x, y) in enumerate(geometry.vertices):
+        plt.text(x, y, f"V{i_vertex}")
+
+    for i_quad, quad in enumerate(geometry.quads):
+        splines = [geometry.edge_splines[i_edge] for i_edge in quad]
+        midpoints = [spline.points[spline.n_points // 2] for spline in splines]
+        centroid = torch.stack(midpoints).mean(dim=0)
+        plt.text(centroid[0], centroid[1], f"Q{i_quad}")
+        for midpoint in midpoints:
+            x, y = torch.stack((centroid, midpoint)).T
+            plt.plot(x, y, color="k", lw=1, ls="dotted")
+
+    plt.gca().axis("equal")
     plt.show()
 
 
