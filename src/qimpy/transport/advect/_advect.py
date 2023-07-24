a631126e68d3315a3ee8f49e7524a1bc3c508e1b
Josh Quinton <josh@quinton.org>
2023-07-24 11:55:16 -0400

    Testing flat transformation and convergence for transport

--- src/qimpy/transport/advect/_advect.py
+++ src/qimpy/transport/advect/_advect.py
@@ -162,14 +162,14 @@ class Advect(Geometry):
     def custom_transformation(self, Q, kx=1, ky=1, amp=-0.05):
         L = torch.tensor([self.Lx, self.Ly], device=qp.rc.device)
         k = torch.tensor([kx, ky], device=qp.rc.device)
-        N = torch.tensor([self.Nx, self.Ny], device=qp.rc.device)
+        N = torch.tensor([self.Nx+2*self.N_ghost, self.Ny+2*self.N_ghost], device=qp.rc.device)
         Nx, Ny, _ = Q.shape
         grad_q = torch.tile(
             torch.eye(2, device=qp.rc.device)[:, None, None], (1, Nx, Ny, 1)
         )
         Q.requires_grad = True
         Q_by_N = Q / N
-        q = L * (Q_by_N + amp * torch.sin(2 * np.pi * k * torch.roll(Q_by_N, 1)))
+        q = L * (Q_by_N )#+ amp * torch.sin(2 * np.pi * k * torch.roll(Q_by_N, 1)))
 
         jacobian = torch.autograd.grad(
             q, Q, grad_outputs=grad_q, is_grads_batched=True
--- src/qimpy/transport/advect/test_advect.py
+++ src/qimpy/transport/advect/test_advect.py
@@ -3,8 +3,7 @@ from _advect import Advect
 import torch
 import numpy as np
 
-
-def convergence(Nxy, N_theta, diag=True):
+def movie(Nxy, N_theta, diag=True):
     import matplotlib.pyplot as plt
 
     qp.utils.log_config()
@@ -38,6 +37,57 @@ def convergence(Nxy, N_theta, diag=True):
 
     for time_step in range(time_steps):
         qp.log.info(f"{time_step = }")
+        plt.clf()
+        sim.plot_streamlines(plt, dict(levels=100), dict(linewidth=1.0))
+        plt.gca().set_aspect("equal")
+        plt.savefig(
+           f"advect_animation/blob_advect_{time_step:04d}.png",
+           bbox_inches="tight",
+           dpi=200,
+        )
+        sim.time_step()
+
+    # Plot only at end (for easier performance benchmarking of time steps):
+    qp.log.info("Plotting density and streamlines")
+
+    qp.utils.StopWatch.print_stats()
+    diff = float(((density_init - sim.density) ** 2).sum() / (sim.Nx * sim.Ny))
+    return diff
+
+def convergence(Nxy, N_theta, diag=True):
+    import matplotlib.pyplot as plt
+
+    qp.utils.log_config()
+    qp.rc.init()
+    assert qp.rc.n_procs == 1  # MPI not yet supported
+
+    v_F = 200.0
+    Lx = 1.0
+    Ly = 1.0
+    sim = Advect(
+        reflect_boundaries=False,
+        contact_width=0.0,
+        v_F=v_F,
+        Lx=Lx,
+        Ly=Ly,
+        Nx=Nxy,
+        Ny=Nxy,
+        N_theta=N_theta,
+        init_angle=np.pi / 4 - np.pi/N_theta if diag else 0.0,
+    )
+    sigma = 0.05
+    sim.rho[:, :, 0] = torch.exp(
+        -((sim.q[:, :, 0] - sim.Lx / 2) ** 2 + (sim.q[:, :, 1] - sim.Ly / 2) ** 2)
+        / sigma**2
+    ).detach()
+    density_init = torch.clone(sim.density)
+
+    t_final = (Lx**2 + Ly**2) ** 0.5 / v_F if diag else Lx / v_F
+    time_steps = round(t_final / sim.dt)
+    print(f"Running for {time_steps} steps.")
+
+    for time_step in range(time_steps):
+        qp.log.info(f"{time_step = }")
         # plt.clf()
         # sim.plot_streamlines(plt, dict(levels=100), dict(linewidth=1.0))
         # plt.gca().set_aspect("equal")
@@ -57,15 +107,16 @@ def convergence(Nxy, N_theta, diag=True):
 
 
 def main():
-    errors = dict()
-    Nthetas = [2, 4, 256]
-    Ns = [64, 128, 256, 512, 1024]
-    for i in Nthetas:
-        for j in Ns:
-            if not (i == 256 and j == 1024):
-                errors[(i, j)] = convergence(j, i, diag=(True if i > 2 else False))
-                print(i, j, errors[(i, j)])
-    print(errors)
+    movie(256, 256, diag=True)
+    #errors = dict()
+    #Nthetas = [2, 4, 256]
+    #Ns = [64, 128, 256, 512]
+    #for i in Nthetas:
+    #    for j in Ns:
+    #        if not (i == 256 and j == 1024):
+    #            errors[(i, j)] = convergence(j, i, diag=(True if i > 2 else False))
+    #            print(i, j, errors[(i, j)])
+    #print(errors)
 
 
 if __name__ == "__main__":
