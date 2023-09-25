a7b50b917cf039d06f73ef8158eaf418e892e14b
Ravishankar Sundararaman <shankar.progs2@gmail.com>
2023-09-25 16:02:27 -0400

    Proposed fix to typing of Default

--- src/qimpy/io/_default.py
+++ src/qimpy/io/_default.py
@@ -1,20 +1,26 @@
 from typing import Generic, Union, TypeVar
 from dataclasses import dataclass
 
-T = TypeVar("T", covariant=True)
+
+T = TypeVar("T")
+
 
 @dataclass(frozen=True)
 class Default(Generic[T]):
     value: T
 
-def unwrap_default(item: Union[T,Default[T]]) -> T:
+
+OptionalDefault = Union[T, Default[T]]
+
+
+def unwrap_default(item: OptionalDefault[T]) -> T:
     if isinstance(item, Default):
         return item.value
     else:
         return item
 
-def test_default() -> bool:
-    a = Default(False)
-    reveal_type(a)
-    return unwrap_default(a)
 
+def test_default(param: OptionalDefault[bool] = Default(False)) -> None:
+    is_default = isinstance(param, Default)
+    value = unwrap_default(is_default)
+    print(f"param = {value} was {'' if is_default else 'not '}specified as a default")
