5c723aefcb20bab3f8ccf79905be98c01ceafe08
Ravishankar Sundararaman <shankar.progs2@gmail.com>
2023-10-27 15:56:25 -0400

    Simplified vertex welding and edge indexing logic

--- src/qimpy/transport/test_spline.py
+++ src/qimpy/transport/test_spline.py
@@ -1,53 +1,45 @@
 from __future__ import annotations
 import argparse
+from xml.dom import minidom
 
 import torch
 import numpy as np
-from xml.dom import minidom
 from svg.path import parse_path, CubicBezier, Line, Close
 
 from qimpy import rc
 
 
+def weld_points(coords: torch.Tensor, tol: float) -> tuple[torch.Tensor, torch.Tensor]:
+    """Weld `coords` within tolerance `tol`, returning indices and unique coordinates.
+    Here, coords has dimensions (..., d), where d is the dimension of space.
+    The first output is a flat list of unique welded vertices of shape (N_uniq, d).
+    The second output contains indices into this unique list of dimensions (...)."""
+    coords_flat = coords.flatten(end_dim=-2)
+    distances = (coords_flat[:, None] - coords_flat[None]).norm(dim=-1)
+    equiv_index = torch.where(distances < tol, 1, 0).argmax(dim=1)
+    _, inverse, counts = torch.unique(
+        equiv_index, return_inverse=True, return_counts=True
+    )
+    # Compute the centroid of each set of equivalent vertices:
+    coords_uniq = torch.zeros((len(counts), coords_flat.shape[-1]), device=rc.device)
+    coords_uniq.index_add_(0, inverse, coords_flat)  # sum equivalent coordinates
+    coords_uniq *= (1.0 / counts)[:, None]  # convert to mean
+    return coords_uniq, inverse.view(coords.shape[:-1])
+
+
+PATCH_SIDES: int = 4  #: Support only quad-patches (implicitly required throughout)
+
+
 class Patches:
-    def __init__(self, splines, epsilon=0.005, patch_sides=4):
-        self.patch_sides = patch_sides
-        # Vertex list
-        # (in complex form for ease of distance calculation)
-        self.vertices = []
-        self.edges = []
+    def __init__(self, splines, epsilon=0.005):
+        self.vertices, self.edges = weld_points(splines[:, (0, -1)], tol=epsilon)
+
         self.cycles = []
-        self.patches = []
-        self.epsilon = epsilon
-
-        def coord_to_complex(coord):
-            return complex(coord[0] + coord[1] * 1j)
-
-        # First pass: Identify all unique vertices (vertex welding)
-        for spline in splines:
-            # Add starting and ending vertex for each spline
-            self.add_vertex(coord_to_complex(spline[0]))
-            self.add_vertex(coord_to_complex(spline[-1]))
-        self.N = len(self.vertices)
-        for spline in splines:
-            # Identify each spline to its respective vertices, building graph
-            i = self.match_vertex(coord_to_complex(spline[0]))
-            j = self.match_vertex(coord_to_complex(spline[-1]))
-            self.edges.append((i, j))
         self.find_cycles()
+
+        self.patches = []
         for cycle in self.cycles:
             self.patches.append([splines[i] for i in cycle])
-
-    def add_vertex(self, new_vertex):
-        if self.match_vertex(new_vertex) is None:
-            self.vertices.append(new_vertex)
-
-    def match_vertex(self, search_vertex):
-        # Uniqueness check
-        for i, vertex in enumerate(self.vertices):
-            if np.abs(vertex - search_vertex) < self.epsilon:
-                return i
-        return None
 
     def add_cycle(self, cycle):
         # Add a cycle if it is unique
@@ -66,26 +58,25 @@ class Patches:
 
     def find_cycles(self):
         # Graph traversal using recursion
-        def cycle_search(self, cycle, depth=1):
-            # Don't bother looking for cycles that exceed a single patch
-            # (limit recursion depth)
-            if depth > self.patch_sides:
+        def cycle_search(cycle, depth=1):
+            # Don't look for cycles that exceed a single patch (limit recursion depth)
+            if depth > PATCH_SIDES:
                 return
             start_vertex = cycle[-1]
             for edge in self.edges:
                 if start_vertex in edge:
                     next_vertex = edge[1] if edge[0] == start_vertex else edge[0]
                     if next_vertex not in cycle:
-                        cycle_search(self, cycle + [next_vertex], depth=depth + 1)
+                        cycle_search(cycle + [next_vertex], depth=depth + 1)
                     elif len(cycle) > 2 and next_vertex == cycle[0]:
                         self.add_cycle(cycle)
 
         # Search for cycles from each starting vertex
-        for start_vertex in list(range(self.N)):
-            cycle_search(self, [start_vertex])
+        for first_vertex in range(len(self.vertices)):
+            cycle_search([first_vertex])
 
 
-def get_splines(svg_file: str) -> list[torch.Tensor]:
+def get_splines(svg_file: str) -> torch.Tensor:
     doc = minidom.parse(svg_file)
     svg_elements = []
     svg_paths = doc.getElementsByTagName("path")
@@ -115,28 +106,30 @@ def get_splines(svg_file: str) -> list[torch.Tensor]:
     # Ignore all elements that are not lines or cubic splines (essentially ignore moves)
     # In the future we may want to throw an error for unsupported segments
     # (e.g. quadratic splines)
-    return [
-        segment_to_tensor(segment)
-        for segment in svg_elements
-        if isinstance(segment, (Line, Close, CubicBezier))
-    ]
+    return torch.stack(
+        [
+            segment_to_tensor(segment)
+            for segment in svg_elements
+            if isinstance(segment, (Line, Close, CubicBezier))
+        ]
+    )
 
 
 def plot_spline(ax, spline: torch.Tensor, n_points: int = 64) -> None:
+    assert len(spline) == 4
     t = np.linspace(0.0, 1.0, n_points + 1)[:, None]
     t_bar = 1.0 - t
     # Evaluate cubic spline by De Casteljau's algorithm:
     control_points = spline.to(rc.cpu).numpy()
     result = control_points[:, None, :]
-    for iter in range(len(spline) - 1):
+    for i_iter in range(len(spline) - 1):
         result = result[:-1] * t_bar + result[1:] * t
     points = result[0]
     # Plot
     ax.plot(points[:, 0], points[:, 1], color="k")
-    if len(spline) == 4:
-        ax.plot(control_points[:2, 0], control_points[:2, 1], color="r")
-        ax.plot(control_points[2:, 0], control_points[2:, 1], color="r")
-        ax.scatter(control_points[1:3, 0], control_points[1:3, 1], color="r")
+    ax.plot(control_points[:2, 0], control_points[:2, 1], color="r")
+    ax.plot(control_points[2:, 0], control_points[2:, 1], color="r")
+    ax.scatter(control_points[1:3, 0], control_points[1:3, 1], color="r")
 
 
 def main():
@@ -151,11 +144,9 @@ def main():
     # Find each patch and print its respective vertices
     patches = Patches(splines)
 
-    def complex_to_list(z):
-        return [z.real, z.imag]
-
     print(f"Found {len(patches.patches)} patches:")
-    [print([complex_to_list(patches.vertices[j]) for j in i]) for i in patches.cycles]
+    for cycle in patches.cycles:
+        print([patches.vertices[j] for j in cycle])
 
     plt.figure()
     ax = plt.gca()
