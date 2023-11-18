df461fde84c3fb01ff8f4c48e33cc2397ebf3da5
Ali Ghorashi <aligho@mit.edu>
2023-11-18 18:37:11 -0500

    slab and tests

--- src/qimpy/grid/_coulomb.py
+++ src/qimpy/grid/_coulomb.py
@@ -187,16 +187,23 @@ class Coulomb:
 
 
 class Coulomb_Slab:
+
     def __init__(self, grid: Grid, n_ions: int, iDir: int) -> None:
         self.iDir = iDir
         self.grid = grid
+        self.update_lattice_dependent(n_ions)
 
     def update_lattice_dependent(self, n_ions: int) -> None:
-        hlfL = 1
-        Gsq = (iG @ grid.lattice.Gbasis.T).square().sum(dim=-1)
-        Gplane = Gsq #- GGT(iDir,iDir) * iG[iDir]*iG[iDir]; //G along the non-truncated directions
-        #Gplane = torch.where(Gplane <= 0, 0, torch.sqrt(Gplane))
-        return (4*np.pi) * (1 - torch.exp(-Gplane*hlfL)) #* torch.cos(np.pi*iG[iDir]))/Gsq 
+        grid = self.grid
+        iDir = self.iDir
+
+        Rsq = (self.grid.lattice.Rbasis).square().sum(dim=0)
+        hlfL = torch.sqrt(Rsq[self.iDir])
+        iG = grid.get_mesh("H").to(torch.double)
+        Gsqi = (iG @ grid.lattice.Gbasis.T).square()
+        Gsq = Gsqi.sum(dim=-1)
+        Gplane = torch.sqrt(Gsq - Gsqi[..., iDir])
+        self._kernel = torch.where(Gsq == 0.0, -0.5*hlfL**2, (4*np.pi) * (1 - torch.exp(-Gplane*hlfL) * torch.cos(np.pi*iG[..., iDir]))/Gsq) 
 
     def __call__() -> None:
         pass
