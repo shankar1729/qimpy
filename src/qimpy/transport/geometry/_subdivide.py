d6d73f5a8d1f97b2b6c14127e27e2b6aaba371c1
Ravishankar Sundararaman <shankar.progs2@gmail.com>
2023-12-16 15:30:53 -0500

    Initial selection / determination of subdivision parameters with load balancing strategies

---
+++ src/qimpy/transport/geometry/_subdivide.py
@@ -0,0 +1,61 @@
+from __future__ import annotations
+
+import numpy as np
+
+from qimpy import log
+from qimpy.math import ceildiv
+from . import QuadSet
+
+
+def divided_count(quad_set: QuadSet, grid_size_max: int) -> tuple[int, float]:
+    """Calculate number of quads if `quad_set` is divided such that
+    the maximum sample count of any resulting quad is `grid_size`.
+    Also return the percentage imbalance between subdivided quad sizes."""
+    n_splits = ceildiv(quad_set.grid_size, grid_size_max)  # 2 splits for each quad
+    n_divided_quads = n_splits.prod(axis=1).sum(axis=0)
+    split_sizes = quad_set.grid_size / n_splits  # approximate: assuming equal splits
+    split_counts = split_sizes.prod(axis=1)
+    imbalance_percent = 100.0 * split_counts.std() / split_counts.mean()
+    return n_divided_quads, imbalance_percent
+
+
+def select_division(quad_set: QuadSet, n_processes: int) -> int:
+    """Select `grid_size_max` suitable for division on `n_processes`.
+    Reports all cases considered to guide selection of parallelization."""
+    # Start with existing sizes, attempting to make domains squarer
+    grid_size_max_list = np.unique(quad_set.grid_size)
+
+    # Expand list with smaller entries if needed
+    n_max = divided_count(quad_set, grid_size_max_list[0])[0]
+    needed_expansion = 4 * n_processes / n_max  # check quad counts till 4 n_processes
+    if needed_expansion > 1.0:
+        log_spacing = 0.2
+        scale_factors = np.exp(
+            -log_spacing - np.arange(0, 0.5 * np.log(needed_expansion), log_spacing)
+        )
+        additional_sizes = np.round(grid_size_max_list[0] * scale_factors)
+        grid_size_max_list = np.unique(
+            np.concatenate((grid_size_max_list, additional_sizes.astype(int)))
+        )
+
+    # Check and report candidates:
+    log.info("\n--- Quad subdivision candidates ---")
+    log.info("grid_size_max  n_quads  size_imbalance number_imbalance")
+    best_grid_size_max = 0
+    best_score = (np.inf,) * 3
+    for grid_size_max in grid_size_max_list[::-1]:
+        n_quads, size_imbalance = divided_count(quad_set, grid_size_max)
+        n_quads_each = ceildiv(n_quads, n_processes)
+        number_imbalance = 100.0 * (1.0 - n_quads / (n_quads_each * n_processes))
+        score = (number_imbalance, size_imbalance, n_quads)
+        if score < best_score:
+            best_score = score
+            best_grid_size_max = grid_size_max
+        log.info(
+            f"{grid_size_max:>9d} {n_quads:>9d}"
+            f" {size_imbalance:>12.0f}% {number_imbalance:>12.0f}%"
+        )
+    log.info(
+        f"Selecting grid_size_max = {best_grid_size_max} for {n_processes} processes"
+    )
+    return best_grid_size_max
--- src/qimpy/transport/geometry/test_svg.py
+++ src/qimpy/transport/geometry/test_svg.py
@@ -1,23 +1,31 @@
 from __future__ import annotations
 import argparse
 
+import matplotlib.pyplot as plt
+
+from qimpy import log, rc
+from qimpy.io import log_config
 from . import parse_svg, plot_spline
+from ._subdivide import select_division
 
 
 def main():
-    import matplotlib.pyplot as plt
+    log_config()
+    rc.init()
+    assert rc.n_procs == 1  # MPI does not make sense for this
 
     parser = argparse.ArgumentParser()
-    parser.add_argument("input_svg", help="Input patch (SVG file)", type=str)
+    parser.add_argument("input_svg", help="Input SVG filename", type=str)
+    parser.add_argument("n_processes", help="# processes to test division", type=int)
     args = parser.parse_args()
 
     quad_set = parse_svg(args.input_svg, grid_spacing=1.0)
 
-    print(f"Found {len(quad_set.quads)} quads:")
+    log.info(f"Found {len(quad_set.quads)} quads:")
     for i_quad, (quad, adjacency, grid_size) in enumerate(
         zip(quad_set.quads, quad_set.adjacency, quad_set.grid_size)
     ):
-        print(f"Quad {i_quad} sampled by {tuple(grid_size)} grid:")
+        log.info(f"Quad {i_quad} sampled by {tuple(grid_size)} grid:")
         for i_edge, edge_index in enumerate(quad):
             i_verts = quad_set.edges[edge_index, [0, -1]]
             v_start, v_stop = quad_set.vertices[i_verts]
@@ -26,13 +34,18 @@ def main():
                 neigh_str = f" (Neighbor: quad {neigh_patch} edge {neigh_edge})"
             else:
                 neigh_str = ""
-            print(f"  Edge {i_edge}: {v_start} -> {v_stop}{neigh_str}")
+            log.info(f"  Edge {i_edge}: {v_start} -> {v_stop}{neigh_str}")
 
     plt.figure()
     ax = plt.gca()
     ax.set_aspect("equal")
     for edge in quad_set.edges:
         plot_spline(ax, quad_set.vertices[edge])
+
+    # Test subdivision:
+    select_division(quad_set, args.n_processes)
+
+    rc.report_end()
     plt.show()
 
 
