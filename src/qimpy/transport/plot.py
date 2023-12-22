8107660d8e70ea9c3de8835386dda132ccc10725
Ravishankar Sundararaman <shankar.progs2@gmail.com>
2023-12-22 13:31:44 -0500

    Added support for collated checkpoints to plot

--- src/qimpy/transport/plot.py
+++ src/qimpy/transport/plot.py
@@ -21,7 +21,12 @@ def main() -> None:
 
     parser = argparse.ArgumentParser()
     parser.add_argument(
-        "checkpoints", help="Filename pattern for checkpoints", type=str
+        "checkpoints", help="Filename pattern (wildcard) for checkpoints", type=str
+    )
+    parser.add_argument(
+        "output",
+        help="Output filename pattern with integer format string (eg. {:04d})",
+        type=str,
     )
     parser.add_argument(
         "--streamlines", help="Whether to draw streamlines", type=bool, default=False
@@ -31,95 +36,102 @@ def main() -> None:
     )
     args = parser.parse_args()
 
-    # Distirbute tasks over MPI:
+    # Distribute tasks over MPI:
     file_list = rc.comm.bcast(sorted(glob.glob(args.checkpoints)))
+    mine = slice(rc.i_proc, None, rc.n_procs)  # divide frames within each file
     geom = PlotGeometry(file_list[0])
     orig_log_level = log.getEffectiveLevel()
     log.setLevel(logging.INFO)  # Capture output from all processes
-    for checkpoint_file in file_list[rc.i_proc :: rc.n_procs]:
-        plot_file = checkpoint_file.replace(".h5", ".png")
-
+    for checkpoint_file in file_list:
         # Load data from checkpoint:
         rho_list = []
         vx_list = []
         vy_list = []
         with h5py.File(checkpoint_file, "r") as cp:
-            n_quads = cp["/geometry/quads"].shape[0]
+            cp_geom = cp["/geometry"]
+            n_quads = cp_geom["quads"].shape[0]
+            i_step_list = np.array(cp_geom.attrs["i_step"])[mine]
+            t_list = np.array(cp_geom.attrs["i_step"])[mine]
             for i_quad in range(n_quads):
-                prefix = f"/geometry/quad{i_quad}"
-                rho_list.append(np.array(cp[f"{prefix}/rho"]))
+                cp_quad = cp_geom[f"quad{i_quad}"]
+                rho_list.append(np.array(cp_quad["rho"][mine]))
                 if args.streamlines:
-                    v = np.array(cp[f"{prefix}/v"])
+                    v = np.array(cp_quad["v"][mine])
                     vx_list.append(v[..., 0])
                     vy_list.append(v[..., 1])
 
-        # Density:
-        plt.clf()
-        rho_max_abs = max(np.max(np.abs(rho)) for rho in rho_list)
-        img = plt.imshow(
-            geom.interpolate(rho_list),
-            extent=geom.extents,
-            origin="lower",
-            cmap="bwr",
-            interpolation="bilinear",
-            vmin=-rho_max_abs,
-            vmax=+rho_max_abs,
-        )
+        # Plot each frame:
+        for i_frame_mine, (i_step, t) in enumerate(zip(i_step_list, t_list)):
+            plot_file = args.output.format(i_step)
+            # Density:
+            plt.clf()
+            plt.title(f"$t$ = {t:.4g}")
+            rho = geom.interpolate(rho_list, i_frame_mine)
+            rho_max_abs = np.max(np.abs(rho))
+            img = plt.imshow(
+                rho,
+                extent=geom.extents,
+                origin="lower",
+                cmap="bwr",
+                interpolation="bilinear",
+                vmin=-rho_max_abs,
+                vmax=+rho_max_abs,
+            )
 
-        # Optional streamlines:
-        if args.streamlines:
-            vx = geom.interpolate(vx_list)
-            vy = geom.interpolate(vy_list)
-            stream_kwargs = dict(color="k", linewidth=1, arrowsize=0.7)
-            if args.transparency:
-                v_mag = np.hypot(vx, vy).filled(0.0)
-                v_rel = np.sqrt(v_mag / v_mag.max())  # suppress low v, but not too much
-                colors = np.zeros((256, 4))
-                colors[:, -1] = np.linspace(v_rel.min(), 1.0, len(colors))
-                alpha_cmap = ListedColormap(colors)
-                stream_kwargs.update(dict(color=v_rel, cmap=alpha_cmap))
-            plt.streamplot(geom.x_grid, geom.y_grid, vx, vy, **stream_kwargs)
+            # Optional streamlines:
+            if args.streamlines:
+                vx = geom.interpolate(vx_list, i_frame_mine)
+                vy = geom.interpolate(vy_list, i_frame_mine)
+                stream_kwargs = dict(color="k", linewidth=1, arrowsize=0.7)
+                if args.transparency:
+                    v_mag = np.hypot(vx, vy).filled(0.0)
+                    v_rel = np.sqrt(v_mag / v_mag.max())  # partially suppress low v
+                    colors = np.zeros((256, 4))
+                    colors[:, -1] = np.linspace(v_rel.min(), 1.0, len(colors))
+                    alpha_cmap = ListedColormap(colors)
+                    stream_kwargs.update(dict(color=v_rel, cmap=alpha_cmap))
+                plt.streamplot(geom.x_grid, geom.y_grid, vx, vy, **stream_kwargs)
 
-        # Draw domain boundaries:
-        for spline in geom.exterior_splines:
-            points = plot_spline(plt.gca(), spline)
+            # Draw domain boundaries:
+            for spline in geom.exterior_splines:
+                points = plot_spline(plt.gca(), spline)
 
-            # Mark contacts if any:
-            for i_contact, contact in enumerate(geom.contacts):
-                center = contact[:2]
-                radius = contact[2]
-                distances = np.linalg.norm(points - center, axis=1)
-                selection = np.where(distances <= radius)[0]
-                if len(selection):
-                    contact_points = points[selection]
-                    plt.plot(contact_points[:, 0], contact_points[:, 1], "w")
-                    i_mid = len(selection) // 2
-                    dq = np.diff(contact_points[i_mid : (i_mid + 2)], axis=0)[0]
-                    angle = np.rad2deg(np.arctan2(dq[1], dq[0]))
-                    plt.text(
-                        *contact_points[i_mid],
-                        geom.contact_names[i_contact],
-                        rotation=angle,
-                        ha="center",
-                        va="top",
-                        rotation_mode="anchor",
-                    )
+                # Mark contacts if any:
+                for i_contact, contact in enumerate(geom.contacts):
+                    center = contact[:2]
+                    radius = contact[2]
+                    distances = np.linalg.norm(points - center, axis=1)
+                    selection = np.where(distances <= radius)[0]
+                    if len(selection):
+                        contact_points = points[selection]
+                        plt.plot(contact_points[:, 0], contact_points[:, 1], "w")
+                        i_mid = len(selection) // 2
+                        dq = np.diff(contact_points[i_mid : (i_mid + 2)], axis=0)[0]
+                        angle = np.rad2deg(np.arctan2(dq[1], dq[0]))
+                        plt.text(
+                            *contact_points[i_mid],
+                            geom.contact_names[i_contact],
+                            rotation=angle,
+                            ha="center",
+                            va="top",
+                            rotation_mode="anchor",
+                        )
 
-        ax = plt.gca()
-        ax.set_aspect("equal")
-        ax.margins(0.1)
-        rho_max_str = r"|\rho|_{\mathrm{max}}"
-        cbar = plt.colorbar(
-            img,
-            ticks=[-rho_max_abs, 0, +rho_max_abs],
-            label=f"Density (${rho_max_str}$ = {rho_max_abs:6.2e})",
-        )
-        cbar.ax.set_yticklabels([rf"$-{rho_max_str}$", "0", rf"$+{rho_max_str}$"])
-        spines = plt.gca().spines
-        spines["top"].set_visible(False)
-        spines["right"].set_visible(False)
-        plt.savefig(plot_file, bbox_inches="tight", dpi=200)
-        log.info(f"Saved {plot_file}")
+            ax = plt.gca()
+            ax.set_aspect("equal")
+            ax.margins(0.1)
+            rho_max_str = r"|\rho|_{\mathrm{max}}"
+            cbar = plt.colorbar(
+                img,
+                ticks=[-rho_max_abs, 0, +rho_max_abs],
+                label=f"Density (${rho_max_str}$ = {rho_max_abs:6.2e})",
+            )
+            cbar.ax.set_yticklabels([rf"$-{rho_max_str}$", "0", rf"$+{rho_max_str}$"])
+            spines = plt.gca().spines
+            spines["top"].set_visible(False)
+            spines["right"].set_visible(False)
+            plt.savefig(plot_file, bbox_inches="tight", dpi=200)
+            log.info(f"Saved {plot_file}")
 
     log.setLevel(orig_log_level)  # Switch log back to single process
     rc.comm.Barrier()
@@ -207,9 +219,11 @@ class PlotGeometry:
         self.x_grid, self.y_grid = np.meshgrid(x_grid_1d, y_grid_1d)
 
     @stopwatch
-    def interpolate(self, values_list: list[np.ndarray]) -> np.ndarray:
+    def interpolate(self, values_list: list[np.ndarray], i_frame: int) -> np.ndarray:
         """Interpolate from geometry to bounding-box grid."""
-        values_all = np.concatenate([values.flatten() for values in values_list])
+        values_all = np.concatenate(
+            [values[i_frame].flatten() for values in values_list]
+        )
         interpolator = mtri.LinearTriInterpolator(self.triangulation, values_all)
         return interpolator(self.x_grid, self.y_grid)
 
