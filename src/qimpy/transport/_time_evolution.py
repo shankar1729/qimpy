f2f917029a77efc02109c2ffe583ffadd8d40274
Ravishankar Sundararaman <shankar.progs2@gmail.com>
2024-03-03 17:50:12 -0500

    Added RK4 option

--- src/qimpy/transport/_time_evolution.py
+++ src/qimpy/transport/_time_evolution.py
@@ -17,6 +17,7 @@ class TimeEvolution(TreeNode):
     n_steps: int  #: Number of steps
     save_interval: int  #: Save results every so many steps
     n_collate: int  #: Collect these many save steps into a single checkpoint
+    integrator: str  #: Time-step style used for integration
 
     def __init__(
         self,
@@ -25,6 +26,7 @@ class TimeEvolution(TreeNode):
         dt_save: float,
         t_max: float,
         n_collate: int,
+        integrator: str = "RK2",
         checkpoint_in: CheckpointPath = CheckpointPath(),
         geometry: Geometry,
     ) -> None:
@@ -40,14 +42,16 @@ class TimeEvolution(TreeNode):
             :yaml:`Time interval at which to save results.`
             This will be rounded to the nearest multiple of `dt` to ensure
             that the results are written at uniform intervals.
+        t_max
+            :yaml:`Stop evolution at this time.`
         n_collate
             :yaml:`Number of save-steps to collect into each checkpoint file.`
             Collecting together several saves can substantially improve performance
             by amortizing the latency associated with disk I/O and GPU transfers.
             The results in the checkpoint have an additional outermost dimension
             corresponding to the number of collated steps.
-        t_max
-            :yaml:`Stop evolution at this time.`
+        integrator
+            :yaml:`Integrator for time-stepping: RK2 or RK4.`
         geometry
             Corresponding geometry from which maximum time step is determined
         """
@@ -64,13 +68,26 @@ class TimeEvolution(TreeNode):
         self.n_steps = max(1, int(np.round(t_max / self.dt)))
         self.save_interval = max(1, int(np.round(dt_save / self.dt)))
         self.n_collate = n_collate
+        self.integrator = integrator
+        if integrator not in {"RK2", "RK4"}:
+            raise InvalidInputException(f"Unrecognized {integrator = }")
 
     def time_step(self, geometry: Geometry) -> None:
         """Second-order correct time step."""
+        t = self.t
         dt = self.dt
         rho0 = geometry.rho
-        rho_half = rho0 + (0.5 * dt) * geometry.rho_dot(rho0, self.t)
-        geometry.rho = rho0 + dt * geometry.rho_dot(rho_half, self.t + 0.5 * dt)
+        if self.integrator == "RK2":
+            rho_half = rho0 + (0.5 * dt) * geometry.rho_dot(rho0, t)
+            geometry.rho = rho0 + dt * geometry.rho_dot(rho_half, t + 0.5 * dt)
+        elif self.integrator == "RK4":
+            k1 = geometry.rho_dot(rho0, t)
+            k2 = geometry.rho_dot(rho0 + (0.5 * dt) * k1, t + 0.5 * dt)
+            k3 = geometry.rho_dot(rho0 + (0.5 * dt) * k2, t + 0.5 * dt)
+            k4 = geometry.rho_dot(rho0 + dt * k3, t + dt)
+            geometry.rho = rho0 + (dt / 6.0) * (k1 + 2 * (k2 + k3) + k4)
+        else:
+            raise KeyError(f"Unrecognized integrator = {self.integrator}")
 
     def run(self, transport: qimpy.transport.Transport) -> None:
         """Run time evolution loop, checkpointing at regular intervals."""
