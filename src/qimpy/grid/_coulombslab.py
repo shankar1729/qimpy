610fa595f85ac66fba285e9ee773f0b840996cb6
Ali Ghorashi <aligho@mit.edu>
2024-05-31 11:46:31 -0400

    Got rid of superfluous tensor dimensions and logging statements

--- src/qimpy/grid/_coulombslab.py
+++ src/qimpy/grid/_coulombslab.py
@@ -39,7 +39,6 @@ class Coulomb_Slab:
 
         hlfL = torch.sqrt(Rsq[iDir]) / 2
         self.hlfL = hlfL
-        log.info(f"hlfL in update_lattice_dependent: {hlfL}")
 
         iG = grid.get_mesh("H").to(torch.double)
         Gi = iG @ grid.lattice.Gbasis.T
@@ -131,7 +130,6 @@ class Coulomb_Slab:
         )
 
         stress_rho2 = FieldH(self.grid, data=(stress_kernel * rho2.data))
-        log.info(f"Stress from CoulombSlab.stress {rho1 ^ stress_rho2}")
         return rho1 ^ stress_rho2
 
     def ewald(self, positions: torch.Tensor, Z: torch.Tensor) -> float:
@@ -142,15 +140,12 @@ class Coulomb_Slab:
         etaSq = eta**2
 
         lattice = self.grid.lattice
-        log.info(f"Positions of ions: {positions} with tensor size {positions.size()}")
-        log.info(f"Charges of ions: {Z} with tensor size {Z.size()}")
         # Position independent terms:
         ZsqTot = (Z**2).sum()
-        Zprod = Z.view(-1, 1, 1) * Z.view(1, -1, 1)
+        Zprod = Z.view(-1, 1) * Z.view(1, -1)
         # Self-energy correction:
 
         E = -ZsqTot * eta * (1 / np.sqrt(np.pi))
-        log.info(f"Self energy term in ewald sum: {E}")
 
         # Next calculate the real space sum:
 
@@ -166,54 +161,61 @@ class Coulomb_Slab:
         rVec = x @ lattice.Rbasis.T  # Cartesian separations for all pairs
         r = rVec.norm(dim=-1)
         r[torch.where(r < rCut)] = grid.N_SIGMAS_PER_WIDTH * sigma
-        Eterm = (0.5 * Zprod * torch.erfc(eta * r) / r).sum()
+        Eterm = 0.5 * torch.einsum("ij, ijk -> ", Zprod, torch.erfc(eta * r) / r)
         E += Eterm
-        minus_E_r_by_r = (
-            Zprod * torch.erfc(eta * r) / r
-            + (2.0 * eta / np.sqrt(np.pi)) * Zprod * torch.exp(-((eta * r) ** 2))
-        ) / (r**2)
+
+        minus_E_r_by_r = torch.einsum(
+            "ij, ijk -> ijk",
+            Zprod,
+            torch.erfc(eta * r) / r**3
+            + (2.0 * eta / np.sqrt(np.pi)) * torch.exp(-((eta * r) ** 2)) / (r**2),
+        )
         # Next calculate reciprocal space sum
         volPrefac = np.pi * 2 * self.hlfL / lattice.volume
         prefac = (
             volPrefac * Zprod
         )  # Factor of two above is to convert halfL to full length in the truncated direction
-        r12 = pos.view(-1, 1, 1, 3) - pos.view(1, -1, 1, 3)
+        r12 = pos.view(-1, 1, 3) - pos.view(1, -1, 3)
         z12 = r12[..., self.iDir]
         z12 *= 2 * self.hlfL
         G = ((self.iG @ lattice.Gbasis.T).square().sum(dim=-1)).sqrt()
 
-        expPlus = torch.exp(G * z12)
+        expPlus = torch.exp(torch.einsum("i, jk -> jki", G, z12))
         expMinus = 1 / expPlus
-        erfcPlus = torch.erfc(eta * (sigmaSq * G + z12))
-        erfcMinus = torch.erfc(eta * (sigmaSq * G - z12))
+        erfcPlus = torch.erfc(
+            eta * (sigmaSq * G.view(1, 1, -1) + z12.view(*z12.size(), 1))
+        )
+        erfcMinus = torch.erfc(
+            eta * (sigmaSq * G.view(1, 1, -1) - z12.view(*z12.size(), 1))
+        )
 
-        c = torch.cos(2 * np.pi * torch.einsum("ijkl, gl->ijkg", r12, self.iG))
-        s = torch.sin(2 * np.pi * torch.einsum("ijkl,gl->ijkg", r12, self.iG))
+        c = torch.cos(2 * np.pi * torch.einsum("ijl, gl -> ijg", r12, self.iG))
+        s = torch.sin(2 * np.pi * torch.einsum("ijl, gl -> ijg", r12, self.iG))
 
-        zTerm = (1 / G) * (expPlus * erfcPlus)
+        zTerm = torch.einsum("i, jki -> jki", 1 / G, expPlus * erfcPlus)
         zTermG0 = -z12 * torch.erf(z12 * eta) - etaSqrtPiInv * torch.exp(
             -etaSq * z12 * z12
         )
-        zTerm_force = (1 / G) * (expPlus * erfcPlus + expMinus * erfcMinus)
+        zTerm_force = torch.einsum(
+            "i, jki -> jki", 1 / G, expPlus * erfcPlus + expMinus * erfcMinus
+        )
         zTerm_prime = expPlus * erfcPlus - expMinus * erfcMinus
 
-        E12 = torch.einsum("ijk, ijkl, ijl-> ", prefac, c, zTerm)
+        E12 = torch.einsum("ij, ijl, ijl-> ", prefac, c, zTerm)
         E12 += (prefac * zTermG0).sum()
         E += E12
         E12_r12 = (
             2
             * np.pi
-            * torch.einsum(
-                "ijk, ijkl, ijl, lm -> im ", prefac, -s, zTerm_force, self.iG
-            )
+            * torch.einsum("ij, ijl, ijl, lm -> im ", prefac, -s, zTerm_force, self.iG)
         )
         E12_r12[:, self.iDir] += (
-            2 * self.hlfL * torch.einsum("ijk, ijkl, ijl -> i", prefac, c, zTerm_prime)
+            2 * self.hlfL * torch.einsum("ij, ijl, ijl -> i", prefac, c, zTerm_prime)
         )
         E12_r12[:, self.iDir] += (
             2
             * self.hlfL
-            * torch.einsum("ijk, ijk -> i", prefac, -2 * torch.erf(z12 * eta))
+            * torch.einsum("ij, ij -> i", prefac, -2 * torch.erf(z12 * eta))
         )
 
         if positions.requires_grad:
@@ -222,6 +224,7 @@ class Coulomb_Slab:
             ).sum(dim=(1, 2))
             reciprocal_sum_forces = E12_r12
             positions.grad += real_sum_forces + reciprocal_sum_forces
+            torch.set_printoptions(19)
 
         if lattice.requires_grad:
             real_sum_stress = -0.5 * torch.einsum(
@@ -237,21 +240,21 @@ class Coulomb_Slab:
             zHat[self.iDir, self.iDir] = 1
             E_RRTzz = (
                 torch.einsum(
-                    "ijk, ijkl, ijl, ijk -> ", prefac, c, 1 / 2 * zTerm_prime, z12
+                    "ij, ijl, ijl, ij -> ", prefac, c, 1 / 2 * zTerm_prime, z12
                 )
                 * zHat
             )
             E_RRTzz += (
-                torch.einsum("ijk, ijk, ijk -> ", prefac, -torch.erf(z12 * eta), z12)
+                torch.einsum("ij, ij, ij -> ", prefac, -torch.erf(z12 * eta), z12)
                 * zHat
             )
             E_RRTzz += E12 * zHat
             minus_zTerm_G_by_G_1 = torch.einsum("ijl, l -> ijl", zTerm, 1 / G**2)
             minus_zTerm_G_by_G_2 = -torch.einsum(
-                "ijl, ijl, l -> ijl", z12, zTerm_prime / 2, 1 / G**2
+                "ij, ijl, l -> ijl", z12, zTerm_prime / 2, 1 / G**2
             )
             minus_zTerm_G_by_G_3 = torch.einsum(
-                "l, ijl -> ijl",
+                "l, ij -> ijl",
                 sigma
                 * np.sqrt(2 / np.pi)
                 * torch.exp(-0.5 * sigmaSq * G**2)
@@ -262,7 +265,7 @@ class Coulomb_Slab:
                 minus_zTerm_G_by_G_1 + minus_zTerm_G_by_G_2 + minus_zTerm_G_by_G_3
             )
             reciprocal_sum_stress_nonvol = torch.einsum(
-                "ijk, ijkl, ijl, la, lb -> ab",
+                "ij, ijl, ijl, la, lb -> ab",
                 prefac,
                 c,
                 minus_zTerm_G_by_G,
@@ -296,7 +299,6 @@ def get_mesh(
     # Create parallelopiped mesh:
     RlengthInv = Gbasis.norm(dim=0).to(rc.cpu) / (2 * np.pi)
     Ncut = 1 + torch.ceil(Rcut * RlengthInv).to(torch.int)
-    log.info(f"Ncut found in get_mesh: {Ncut}")
     Ncut[iDir] = 0  # Do not sum over truncated direction
     iRgrids = tuple(
         torch.arange(-N, N + 1, device=rc.device, dtype=torch.double) for N in Ncut
@@ -304,8 +306,6 @@ def get_mesh(
     iR = torch.stack(torch.meshgrid(*iRgrids, indexing="ij")).flatten(1).T
     if exclude_zero:
         iR = iR[torch.where(iR.abs().sum(dim=-1))[0]]
-    # log.info(f"iR: {iR} with shape {iR.shape}")
-
     # Add margin mesh:
     marginGrid = torch.tensor([-1, 0, +1] if include_margin else [0], device=rc.device)
     iMargin = torch.stack(torch.meshgrid([marginGrid] * 3, indexing="ij")).flatten(1).T
