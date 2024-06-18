Fix broadcasting issue for parameter grid

src/qimpy/transport/material/ab_initio/_light.py
@@ -32,7 +32,7 @@ class Light(TreeNode):
         gauge: str = "velocity",
         A0: Optional[list[complex]] = None,
         E0: Optional[list[complex]] = None,
-        omega: float,
+        omega: float = 0.0,
         t0: float = 0.0,
         sigma: float = 0.0,
         smearing: float = 0.001,
@@ -121,6 +121,7 @@ class Light(TreeNode):
                 "Parameter gauge should only be velocity or length"
             )
 
+        omega = omega * torch.ones([1,1]).to(rc.device) # easy for broadcasting
         self.t0[patch_id] = t0
         self.sigma[patch_id] = sigma
         if self.coherent:
@@ -130,9 +131,13 @@ class Light(TreeNode):
             prefac = torch.sqrt(torch.sqrt(torch.pi / (8 * smearing**2)))
             exp_factor = -1.0 / (smearing**2)
             Nk, Nb = ab_initio.E.shape
-            dE = ab_initio.E.reshape([Nk, Nb, 1]) - ab_initio.E.reshape([Nk, 1, Nb])
-            plus = prefac * amp_mat * torch.exp(exp_factor * ((dE + omega) ** 2))
-            minus = prefac * amp_mat * torch.exp(exp_factor * ((dE - omega) ** 2))
+            dE = ab_initio.E.reshape([1, 1, Nk, Nb, 1]) - ab_initio.E.reshape([1, 1, Nk, 1, Nb])
+            plus = prefac * amp_mat * torch.exp(
+                exp_factor * ((dE + omega_[:, :, None, None, None]) ** 2)
+            )
+            minus = prefac * amp_mat * torch.exp(
+                exp_factor * ((dE - omega_[:, :, None, None, None]) ** 2)
+            )
             plus_deg = plus.swapaxes(-2, -1).conj()
             minus_deg = minus.swapaxes(-2, -1).conj()
             self.identity_mat = torch.tile(torch.eye(Nb), (1, Nk, 1, 1)).to(rc.device)
@@ -146,16 +151,16 @@ class Light(TreeNode):
         t0 = self.t0[patch_id]
         sigma = self.sigma[patch_id]
         if sigma > 0:
-            prefac = torch.exp(-((t - t0) ** 2) / (2 * sigma**2)) / np.sqrt(
-                np.sqrt(np.pi) * sigma
+            prefac = torch.exp(-((t - t0) ** 2) / (2 * sigma**2)) / torch.sqrt(
+                torch.sqrt(np.pi * sigma**2)
             )
         else:
             prefac = 1.0
 
         if self.coherent:
             omega = self.omega[patch_id]
-            prefac *= -0.5j * torch.exp(-1j * omega * t)  # Louiville, symmetrization
-            interaction = prefac * self.amp_mat[patch_id]
+            prefac = -0.5j * torch.exp(-1j * omega * t) * prefac  # Louiville, symmetrization
+            interaction = prefac[:, :, None, None, None] * self.amp_mat[patch_id][None, None, ...]
             return (interaction - interaction.swapaxes(-2, -1).conj()) @ rho
         else:
             prefac = 0.5 * prefac**2

src/qimpy/transport/material/ab_initio/_relaxation_time.py
@@ -105,6 +105,10 @@ class RelaxationTime(TreeNode):
         self.max_dbetamu = max_dmu / ab_initio.T
         self.dbetamu_eps = float(dmu_eps) / ab_initio.T
         self.nbands = ab_initio.E.shape[-1]
+        self.sum_rules = {
+            2: "xykb -> xy", # electron or hole
+            3: "xykb -> xyk", # recombination
+        }
 
     def initialize_fields(self, params: dict[str, torch.Tensor], patch_id: int) -> None:
         self._initialize_fields(patch_id, **params)
@@ -137,7 +141,7 @@ class RelaxationTime(TreeNode):
             self.tau_recomb[patch_id] = tau_recomb
             self.exp_betaE[patch_id] = torch.exp(ab_initio.E / ab_initio.T)
             self.betamuk0[patch_id] = (
-                torch.ones([1, 1, ab_initio.E.shape[0], 1]).to(rc.device) * betamu0
+                torch.ones([1, 1, ab_initio.E.shape[0]]).to(rc.device) * betamu0
             )
 
     @stopwatch
@@ -153,8 +157,6 @@ class RelaxationTime(TreeNode):
                 fe,
                 self.exp_betaEe[patch_id],
                 self.betamue0[patch_id],
-                sum_rule="...kb -> ...",
-                reshape=fe.shape[:-2],
             )
             if self.only_diagonal:
                 result[..., range(nv, nbands), range(nv, nbands)] -= (fe - fe_eq) / (
@@ -172,8 +174,6 @@ class RelaxationTime(TreeNode):
                 1 - fh,
                 self.exp_betaEh[patch_id],
                 self.betamuh0[patch_id],
-                sum_rule="...kb -> ...",
-                reshape=fh.shape[:-2],
             )
             fh_eq = 1 - fh_eq
             if self.only_diagonal:
@@ -197,8 +197,6 @@ class RelaxationTime(TreeNode):
                 fk,
                 self.exp_betaE[patch_id],
                 self.betamuk0[patch_id],
-                sum_rule="...kb -> ...k",
-                reshape=fk.shape[:-1] + (1,),
             )
             result -= (rho - torch.diag_embed(fk_eq)) / (2 * self.tau_recomb[patch_id])
             self.betamuk0[patch_id] = betamuk
@@ -210,26 +208,27 @@ class RelaxationTime(TreeNode):
         f: torch.Tensor,
         exp_betaE: torch.Tensor,
         betamu0: torch.Tensor,
-        sum_rule: str,
-        reshape: tuple = (1,),
     ) -> tuple[torch.Tensor, torch.Tensor]:
-        f_total = torch.einsum(sum_rule, f).reshape(reshape)
         betamu = betamu0
-        distribution = fermi_dirac(exp_betaE, betamu)
-        F = torch.einsum(sum_rule, distribution).reshape(reshape) - f_total
+        sum_rule = self.sum_rules[betamu.ndim]
+        f_total = torch.einsum(sum_rule, f)
+        reshape = betamu.shape + (1,) * (4 - betamu.ndim)
+        # Fermi-dirac distribution, shape (Nx, Ny, Nk, Nb)
+        exp_beta_Emu = exp_betaE[None,None,:,:] / torch.exp(betamu).reshape(reshape)
+        distribution = 1 / (exp_beta_Emu + 1)
+        F = torch.einsum(sum_rule, distribution) - f_total
         while torch.max(torch.abs(F)) > self.eps:
-            exp_beta_Emu = exp_betaE / torch.exp(betamu)
-            dF = torch.einsum(sum_rule, exp_beta_Emu / (exp_beta_Emu + 1) ** 2).reshape(
-                reshape
-            )
+            dF = torch.einsum(sum_rule, exp_beta_Emu / (exp_beta_Emu + 1) ** 2)
             dbetamu = F / dF
             limit_ind = (
                 torch.abs(dbetamu) > self.max_dbetamu
             )  # indices that need to be limited by self.max_dbetamu
             dbetamu[limit_ind] = torch.sign(F[limit_ind]) * self.max_dbetamu
-            betamu -= dbetamu
-            distribution = fermi_dirac(exp_betaE, betamu)
-            F = torch.einsum(sum_rule, distribution).reshape(reshape) - f_total
+            betamu = betamu - dbetamu
+            reshape = betamu.shape + (1,) * (4 - betamu.ndim) # handle broadcasting
+            exp_beta_Emu = exp_betaE[None,None,:,:] / torch.exp(betamu).reshape(reshape)
+            distribution = 1 / (exp_beta_Emu + 1)
+            F = torch.einsum(sum_rule, distribution) - f_total
             if torch.max(torch.abs(dbetamu)) < self.dbetamu_eps:
                 break
         return betamu, distribution
@@ -248,8 +247,3 @@ class RelaxationTime(TreeNode):
         )
         return ["tau_p", "tau_s", "tau_e", "tau_h", "tau_eh", "tau_recomb",
                 "nv", "max_dmu", "eps", "dmu_eps", "only_diagonal"]
-
-
-def fermi_dirac(exp_betaE: torch.Tensor, betamu: torch.Tensor) -> torch.Tensor:
-    expbetamu = torch.exp(betamu)
-    return 1 / (exp_betaE / expbetamu + 1)