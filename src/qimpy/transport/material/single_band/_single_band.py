41785e62b1c687b53df6aa0c893f13f7deb9930d
Ravishankar Sundararaman <shankar.progs2@gmail.com>
2025-12-11 15:32:27 -0500

    First basic working draft of single band material with ballistic transport only (no scattering or reflections yet)

--- src/qimpy/transport/material/single_band/_single_band.py
+++ src/qimpy/transport/material/single_band/_single_band.py
@@ -1,5 +1,6 @@
 from __future__ import annotations
-from typing import Union, Sequence, Callable
+from typing import Union, Sequence, Callable, Optional
+from functools import cache
 from dataclasses import dataclass
 
 import numpy as np
@@ -7,7 +8,7 @@ import torch
 
 from qimpy import rc, log
 from qimpy.mpi import ProcessGrid
-from qimpy.io import CheckpointPath
+from qimpy.io import CheckpointPath, CheckpointContext
 from qimpy.lattice import Lattice
 from .. import Material, fermi
 
@@ -20,7 +21,9 @@ class SingleBand(Material):
         *,
         lattice: Union[Lattice, dict],
         kmesh: Union[Sequence[int], np.ndarray],
-        dispersion: Union[dict, Callable[[torch.Tensor], torch.Tensor]],
+        v: float = 0.0,
+        m: float = 0.0,
+        dispersion: Optional[Callable[[torch.Tensor], torch.Tensor]] = None,
         mu: float,
         T: float,
         nT_below: float = 5,
@@ -36,16 +39,19 @@ class SingleBand(Material):
             :yaml:`Bulk lattice vectors / unit cell definition.`
         kmesh
             :yaml:`Uniform k-point mesh from which to select relevant k-points.`
+        v
+            :yaml:`Velocity specifying a linear dispersion model.`
+            Exactly only one of v, m and dispersion must be specified.
+        m
+            :yaml:`Effective mass specifying a quadratic dispersion model.`
+            Exactly only one of v, m and dispersion must be specified.
         dispersion
-            :yaml:`Dispersion relation for the single band model.`
-            Supported options include:
-            * `v = value` for a linear dispersion relation with velocity `m`
-            * `m = value` for a quadratic dispersion relation with effective mass `m`
-            * In code, custom function that returns energy given Cartesian k
-              (in atomic units) and the lattice. For convenience, the Cartesian k
-              will be pre-wrapped aroung k = 0. In principle the energy function
-              should be periodic on the reciprocal lattice, but this will not matter
-              if the selected k are far from the Brillouin zone boundaries.
+            In code, custom function that returns energy given Cartesian k
+            (in atomic units) and the lattice. For convenience, the Cartesian k
+            will be pre-wrapped aroung k = 0. In principle the energy function
+            should be periodic on the reciprocal lattice, but this will not matter
+            if the selected k are far from the Brillouin zone boundaries.
+            This option currently does not support continuing from checkpoints.
         mu
             :yaml:`Backround/reference chemical potential of initial state`
         T
@@ -64,14 +70,14 @@ class SingleBand(Material):
         self.nT_above = nT_above
 
         # Prepare dispersion relation:
-        if isinstance(dispersion, dict):
-            if "v" in dispersion:
-                self.dispersion = LinearDispersion(**dispersion)
-            elif "m" in dispersion:
-                self.dispersion = QuadraticDispersion(**dispersion)
-            else:
-                raise KeyError("Unrecognized dispersion relation")
-        else:
+        if v:
+            assert (not m) and (dispersion is None)
+            self.dispersion = LinearDispersion(v=v)
+        if m:
+            assert (not v) and (dispersion is None)
+            self.dispersion = QuadraticDispersion(m=m)
+        if dispersion is not None:
+            assert (not v) and (not m)
             assert callable(dispersion)
             self.dispersion = dispersion
 
@@ -117,15 +123,77 @@ class SingleBand(Material):
         self.v[:, 0, :] = k_cart.grad.detach()
         self.rho0[:, 0, 0] = fermi(self.E[:, 0], mu, T)
 
-        from matplotlib import pyplot as plt
+    def _save_checkpoint(
+        self, cp_path: CheckpointPath, context: CheckpointContext
+    ) -> list[str]:
+        attrs = cp_path.attrs
+        attrs["kmesh"] = self.kmesh
+        if isinstance(self.dispersion, LinearDispersion):
+            attrs["v"] = self.v
+        if isinstance(self.dispersion, QuadraticDispersion):
+            attrs["m"] = self.m
+        attrs["mu"] = self.mu
+        attrs["T"] = self.T
+        attrs["nT_below"] = self.nT_below
+        attrs["nT_above"] = self.nT_above
+        return list(attrs.keys())
 
-        kx, ky, _ = k_cart.detach().T
-        f = self.rho0[:, 0, 0]
-        plt.scatter(kx, ky, c=f * (1 - f) * self.v[:, 0, 1], cmap="viridis")
-        plt.gca().set_aspect("equal")
-        plt.colorbar()
-        plt.show()
-        exit()
+    def initialize_fields(
+        self, rho: torch.Tensor, params: dict[str, torch.Tensor], patch_id: int
+    ) -> None:
+        pass  # No spatially-varying / parameter sweep fields yet
+
+    def get_contactor(
+        self, n: torch.Tensor, **kwargs
+    ) -> Callable[[float], torch.Tensor]:
+        return Contactor(self, n, **kwargs)
+
+    def get_reflector(
+        self, n: torch.Tensor
+    ) -> Callable[[torch.Tensor], torch.Tensor]:  # absorbing boundary
+        return torch.zeros_like
+
+    def rho_dot(self, rho: torch.Tensor, t: float, patch_id: int) -> torch.Tensor:
+        return torch.zeros_like(rho)  # TODO: add scattering
+
+    def get_observable_names(self) -> list[str]:
+        return ["n", "jx", "jy", "e"]  # density, fluxes and energy density
+
+    @cache
+    def get_observables(self, t: float) -> torch.Tensor:
+        return torch.cat(
+            (
+                torch.ones((1, self.nk_mine), device=rc.device),
+                self.transport_velocity.T,
+                self.E.T,
+            ),
+            dim=0,
+        )
+
+
+class Contactor:
+    rho_contact: torch.Tensor  #: Cached constant contact distribution
+
+    def __init__(
+        self,
+        sb: SingleBand,
+        n: torch.Tensor,
+        *,
+        dmu: float = 0.0,
+        dT: float = 0.0,
+        Eeff: float = 0.0,
+    ) -> None:
+        """Return contact distribution function for specified chemical potential
+        shift `dmu`, temperature shift `dT` and Fermi surface tilt parameterized
+        by effective electric field `Eeff` along the normal direction `n`.
+        (Eeff is the Lagrange multiplier corresponding to a drift velocity.)"""
+        self.rho_contact = fermi(
+            sb.E[:, 0] + Eeff * (n @ sb.transport_velocity.T), sb.mu + dmu, sb.T + dT
+        )
+
+    def __call__(self, t):
+        # TODO: add time dependence options
+        return self.rho_contact
 
 
 @dataclass
