Fixed behavior of contacts with masking

src/qimpy/transport/advect.py
@@ -1,5 +1,5 @@
 from __future__ import annotations
-from typing import Any
+from typing import Any, Optional
 
 import torch
 from qimpy import rc
@@ -54,13 +54,26 @@ class Advect(torch.nn.Module):
         return slope.unflatten(0, batch_shape)  # restore dimensions
 
     def forward(
-        self, rho: torch.Tensor, v: torch.Tensor, axis: int
+        self,
+        rho: torch.Tensor,
+        v: torch.Tensor,
+        axis: int,
+        edge_masks: tuple[Optional[torch.Tensor], Optional[torch.Tensor]] = (
+            None,
+            None,
+        ),
     ) -> list[torch.Tensor]:
         """Compute advection -d`v``rho`/dx of `rho` with velocity `v` along `axis`.
         Along `axis`, for a domain of actual length N, `rho` and `v` are ghost-padded
         with length N + 2 if `pad`, and N + 4 otherwise. The result contains the domain
         contribution, along with left and right edge contributions if `pad` is True.
         All tensors have equal/broadcastable dimensions along all other axes.
+
+        If specified, edge_masks control where the incoming flux is zeroed out on the
+        left and right boundaries. For the typical use case with `pad` = True, this
+        mask should zero out the incoming flux, except in contact regions.
+        If the masks are None, the flux is not zeroed out and can lead to double
+        counting with the edge contributions for the `pad` = True case.
         """
         # Bring active axis to end
         flux = (rho * v).swapaxes(axis, -1)
@@ -78,8 +91,14 @@ class Advect(torch.nn.Module):
         flux_plus = (flux + half_slope)[..., :-1]  # + flux from prev cell
         flux_minus[v[..., 1:] >= 0.0] = 0.0  # select v < 0 contributions
         flux_plus[v[..., :-1] <= 0.0] = 0.0  # select v > 0 contributions
-        flux_minus[..., -1] = 0.0
-        flux_plus[..., 0] = 0.0
+        for flux_sign, edge_mask, index in zip(
+            (flux_plus, flux_minus), edge_masks, (0, -1)
+        ):
+            if edge_mask is not None:
+                if axis == 0:
+                    flux_sign[:, edge_mask, index] = 0.0
+                if axis == 1:
+                    flux_sign[edge_mask, :, index] = 0.0
         flux_net = flux_minus + flux_plus
         out = [-flux_net.diff(dim=-1).swapaxes(axis, -1)]
         if self.pad:

src/qimpy/transport/geometry/_patch.py
@@ -25,7 +25,8 @@ class Patch:
 
     q: torch.Tensor  #: Nx x Ny x 2 Cartesian coordinates
     g: torch.Tensor  #: Nx x Ny x 1 sqrt(metric), with extra dimensipm for broadcasting
-    V: torch.Tensor  #: Nx_padded x Ny_padded x Nkbb x 2 mesh coordinate velocities
+    V0: torch.Tensor  #: Nx_padded x Ny x Nkbb mesh coordinate velocities in first axis
+    V1: torch.Tensor  #: Nx x Ny_padded x Nkbb mesh coordinate velocities in second axis
     dt_max: float  #: Maximum stable time step
     wk: float  #: Integration weight for the flattened density matrix dimensions
     rho_offset: tuple[int, ...]  #: Offset of density matrix data within that of quad
@@ -40,6 +41,8 @@ class Patch:
         Optional[Callable[[torch.Tensor], torch.Tensor]]
     ]  #: Material-dependent reflector for each edge that needs one
     contacts: list[list[Contact]]  #: Contact calculators (multiple possibly) by edge
+    edge_masks0: list[torch.Tensor]  #: Masks for zeroing edge flux in first axis
+    edge_masks1: list[torch.Tensor]  #: Masks for zeroing edge flux in second axis
 
     def __init__(
         self,
@@ -109,11 +112,12 @@ class Patch:
             self.rho = torch.tile(material.rho0.flatten(), (N[0], N[1], 1))
 
         # Store mesh velocities with padding needed for advection:
-        self.V = torch.nn.functional.pad(V, (0,) * 4 + (N_GHOST,) * 4)
-        self.V[0, NON_GHOST] = V[0]
-        self.V[-1, NON_GHOST] = V[-1]
-        self.V[NON_GHOST, 0] = V[:, 0]
-        self.V[NON_GHOST, -1] = V[:, -1]
+        self.V0 = torch.nn.functional.pad(V[..., 0], (0, 0, 0, 0, N_GHOST, N_GHOST))
+        self.V0[0] = V[0, ..., 0]
+        self.V0[-1] = V[-1, ..., 0]
+        self.V1 = torch.nn.functional.pad(V[..., 1], (0, 0, N_GHOST, N_GHOST))
+        self.V1[:, 0] = V[:, 0, :, 1]
+        self.V1[:, -1] = V[:, -1, :, 1]
 
         # Initialize v*drho/dx calculator:
         self.advect = torch.jit.script(
@@ -125,14 +129,18 @@ class Patch:
         self.aperture_selections = [None] * 4
         self.reflectors = [None] * 4
         self.contacts = [[] for _ in range(4)]  # Note: [[]]*N makes N refs to one []!
+        edge_masks = []
         for i_edge, (is_reflective_i, has_apertures_i) in enumerate(
             zip(is_reflective, has_apertures)
         ):
-            if not (is_reflective_i or has_apertures_i):
-                continue  # Entirely pass-through (neither reflective nor has apertures)
-
             i_dim = i_edge % 2  # long direction of edge
             j_dim = 1 - i_dim  # other direction
+
+            edge_mask = torch.full((N[i_dim],), True, device=rc.device)
+            edge_masks.append(edge_mask)
+
+            if not (is_reflective_i or has_apertures_i):
+                continue  # Entirely pass-through (neither reflective nor has apertures)
 
             # Compute coordinates along edge:
             Q_edge = torch.empty((N[i_dim], 2), device=rc.device)
@@ -182,6 +190,10 @@ class Patch:
                         normal[contact_slice], **contact_params_i
                     )
                     self.contacts[i_edge].append(Contact(contact_slice, contactor))
+                    edge_mask[contact_slice] = False
+
+        self.edge_masks0 = [edge_masks[3], edge_masks[1]]
+        self.edge_masks1 = [edge_masks[0], edge_masks[2]]
 
     def save_checkpoint(
         self, cp_path: CheckpointPath, observables: torch.Tensor, save_rho: bool
@@ -205,10 +217,8 @@ class Patch:
         """Compute g*rho_dot, given current g*rho.
         The input is ghost-padded, while the output contains the contributions within
         the domain and the edge contributions that must be reflected/passed-through."""
-        V0 = self.V[:, NON_GHOST, :, 0]
-        V1 = self.V[NON_GHOST, :, :, 1]
-        out0, outL, outR = self.advect(grho[:, NON_GHOST], V0, axis=0)
-        out1, outB, outT = self.advect(grho[NON_GHOST, :], V1, axis=1)
+        out0, outL, outR = self.advect(grho[:, NON_GHOST], self.V0, 0, self.edge_masks0)
+        out1, outB, outT = self.advect(grho[NON_GHOST, :], self.V1, 1, self.edge_masks1)
         return out0 + out1, [outB, outR, outT, outL]
 
 