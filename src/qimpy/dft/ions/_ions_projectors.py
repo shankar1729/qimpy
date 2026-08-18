Fixed force and lowdin calculation: complete DFT calculation now working with dist!

src/qimpy/dft/ions/_ions_projectors.py
@@ -2,10 +2,10 @@ from __future__ import annotations
 
 import numpy as np
 import torch
+import torch.distributed as dist
 
-from qimpy import rc, dft, MPI
+from qimpy import rc, dft
 from qimpy.profiler import stopwatch
-from qimpy.mpi import BufferView
 from qimpy.math import RadialFunction, spherical_harmonics as sh
 from qimpy.math.quintic_spline import Interpolator
 from qimpy.dft import ions
@@ -122,7 +122,7 @@ def projectors_grad(
             # Prepare for next species:
             i_proj_start = i_proj_stop
 
-        basis.kpoints.comm.Allreduce(MPI.IN_PLACE, BufferView(pos_grad))
+        dist.all_reduce(pos_grad, group=basis.kpoints.group)
         self.positions.grad += pos_grad
 
     # Projector stress:
@@ -176,5 +176,5 @@ def projectors_grad(
             # Prepare for next species:
             i_proj_start = i_proj_stop
 
-        basis.kpoints.comm.Allreduce(MPI.IN_PLACE, BufferView(lattice_grad))
+        dist.all_reduce(lattice_grad, group=basis.kpoints.group)
         self.lattice.grad += lattice_grad

src/qimpy/dft/ions/_lowdin.py
@@ -3,10 +3,10 @@ from typing import NamedTuple, Optional
 
 import numpy as np
 import torch
+import torch.distributed as dist
 
-from qimpy import rc, dft, MPI
+from qimpy import rc, dft
 from qimpy.profiler import stopwatch
-from qimpy.mpi import BufferView
 from qimpy.math import ortho_matrix, cis, abs_squared
 
 
@@ -50,7 +50,7 @@ class Lowdin:
         if spin_polarized and self.C.spinorial:
             # Need off-diagonal density matrix components for spinorial magnetization:
             Rho = torch.einsum("skab, skb, skAb -> aA", lowdin, wf, lowdin.conj())
-            basis.kpoints.comm.Allreduce(MPI.IN_PLACE, BufferView(Rho))
+            dist.all_reduce(Rho, group=basis.kpoints.group)
             result = torch.empty((4, ions.n_ions), device=rc.device)
             i_psi_start = 0
             for slice_i, ps in zip(ions.slices, ions.pseudopotentials):
@@ -72,7 +72,7 @@ class Lowdin:
         else:
             # Diagonal components of density matrix suffice:
             Rho = torch.einsum("skb, skab -> sa", wf, abs_squared(lowdin))
-            basis.kpoints.comm.Allreduce(MPI.IN_PLACE, BufferView(Rho))
+            dist.all_reduce(Rho, group=basis.kpoints.group)
             # Reduce to (spin)-number on each atom:
             Ns = torch.zeros((Rho.shape[0], ions.n_ions), device=rc.device)
             Ns.index_add_(1, i_ion, Rho)